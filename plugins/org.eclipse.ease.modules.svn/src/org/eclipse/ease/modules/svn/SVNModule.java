bd767438f38ba3c8cb4d0d38dee70385b9c8a638
dominic.pirker@gmail.com <dominic.pirker@gmail.com>
2015-02-25 14:13:37 +0100

    Bug 460788: contribution of svn module
      contribute a svn module to create repository locations, to checkout
      and to import projects from svn
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=460788
    Change-Id: Ie20e3b8a5c6a6c5f3660397f499e985b90c136e1
    Signed-off-by: dominic.pirker@gmail.com <dominic.pirker@gmail.com>

---
+++ plugins/org.eclipse.ease.modules.svn/src/org/eclipse/ease/modules/svn/SVNModule.java
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dominic Pirker and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dominic Pirker - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ease.modules.svn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.ease.modules.ScriptParameter;
+import org.eclipse.ease.modules.WrapToScript;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.team.svn.core.connector.SVNDepth;
+import org.eclipse.team.svn.core.operation.CompositeOperation;
+import org.eclipse.team.svn.core.operation.IActionOperation;
+import org.eclipse.team.svn.core.operation.remote.management.AddRepositoryLocationOperation;
+import org.eclipse.team.svn.core.operation.remote.management.SaveRepositoryLocationsOperation;
+import org.eclipse.team.svn.core.resource.IRepositoryLocation;
+import org.eclipse.team.svn.core.resource.IRepositoryResource;
+import org.eclipse.team.svn.core.svnstorage.SVNRemoteStorage;
+import org.eclipse.team.svn.ui.extension.ExtensionsManager;
+import org.eclipse.team.svn.ui.utility.UIMonitorUtility;
+
+public class SVNModule {
+
+	public SVNModule() {
+	}
+
+	/**
+	 * Creates repository location.
+	 * 
+	 * Creates repository location with rooturl, if location already exists: return existing location
+	 * 
+	 * @param rootUrl
+	 *            defines the root URL of the repository
+	 * @param username
+	 *            registered username of the repository
+	 * @param password
+	 *            password depending to the username
+	 * @return repository location which is created, or the existing repository location
+	 */
+	@WrapToScript
+	public IRepositoryLocation createRepositoryLocation(final String rootUrl, @ScriptParameter(defaultValue = ScriptParameter.NULL) final String username,
+			@ScriptParameter(defaultValue = ScriptParameter.NULL) final String password) {
+
+		IRepositoryLocation[] locations = SVNRemoteStorage.instance().getRepositoryLocations();
+
+		for (IRepositoryLocation location : locations) {
+			if ((location.getUrlAsIs().equals(rootUrl)) || (location.getUrl().equals(rootUrl))) {
+				return location;
+			}
+		}
+
+		IRepositoryLocation location = SVNRemoteStorage.instance().newRepositoryLocation();
+
+		location.setUrl(rootUrl);
+		location.setTrunkLocation("trunk");
+		location.setTagsLocation("tags");
+		location.setBranchesLocation("branches");
+		location.setStructureEnabled(true);
+
+		if (username != null)
+			location.setUsername(username);
+		if (password != null)
+			location.setPassword(password);
+
+		location.setPasswordSaved(true);
+
+		AddRepositoryLocationOperation operation = new AddRepositoryLocationOperation(location);
+		final CompositeOperation op = new CompositeOperation(operation.getId(), operation.getMessagesClass());
+		op.add(operation);
+		op.add(new SaveRepositoryLocationsOperation());
+
+		Display.getDefault().syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				UIMonitorUtility.doTaskNowDefault(op, false);
+			}
+		});
+
+		return location;
+	}
+
+	/**
+	 * Imports project from repository location.
+	 * 
+	 * Imports project (given with projectLocation) from repository location.
+	 * 
+	 * @param rootLocation
+	 *            can be a string (-> generate RepositoryLocation automatically) or already a RepositoryLocation
+	 * @param projectLocations
+	 *            array from relative paths to project locations
+	 */
+	@WrapToScript
+	public void importProjectFromSVN(Object rootLocation, String[] projectLocations) throws Exception {
+			if (rootLocation instanceof IRepositoryResource) {
+			} else {
+				rootLocation = createRepositoryLocation(rootLocation.toString(), null, null);
+			}
+
+			List<IRepositoryResource> doCeckout_tmp = new ArrayList<IRepositoryResource>();
+			for (String location : projectLocations) {
+				IRepositoryResource projectResource = SVNRemoteStorage.instance().asRepositoryResource((IRepositoryLocation) rootLocation,
+						((IRepositoryLocation) rootLocation).getUrl() + "/" + location, false);
+				doCeckout_tmp.add(projectResource);
+			}
+
+			final IRepositoryResource[] doCeckout = doCeckout_tmp.toArray(new IRepositoryResource[] {});
+
+			Display.getDefault().syncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					Shell sh = Display.getDefault().getActiveShell();
+					IActionOperation op = ExtensionsManager.getInstance().getCurrentCheckoutFactory()
+							.getCheckoutOperation(sh, doCeckout, null, true, null, SVNDepth.INFINITY, false);
+					UIMonitorUtility.doTaskNowDefault(op, true);
+				}
+			});
+	}
+}

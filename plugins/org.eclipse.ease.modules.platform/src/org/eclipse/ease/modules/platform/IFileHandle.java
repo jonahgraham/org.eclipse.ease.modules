37927281901131a05f2c183dad48d81a0d4695ab
Christian Pontesegger <christian.pontesegger@web.de>
2015-03-24 15:43:41 +0100

    added javadoc
    Change-Id: Id8ec48d05b3cbaa66fa910fb9c4771e0b9a76fd9

--- plugins/org.eclipse.ease.modules.platform/src/org/eclipse/ease/modules/platform/IFileHandle.java
+++ plugins/org.eclipse.ease.modules.platform/src/org/eclipse/ease/modules/platform/IFileHandle.java
@@ -15,21 +15,71 @@ import java.io.IOException;
 
 import org.eclipse.core.resources.IFile;
 
+/**
+ * Generic handle to an {@link IFile} or {@link File} instance.
+ */
 public interface IFileHandle {
+
+	/** Open file in read mode. */
 	int READ = 1;
+
+	/** Open file in write mode. */
 	int WRITE = 2;
+
+	/** Open file in append mode. */
 	int APPEND = 4;
 
+	/**
+	 * Read characters from a file.
+	 *
+	 * @param characters
+	 *            amount of characters to read
+	 * @return data read from file
+	 * @throws IOException
+	 *             on access errors
+	 */
 	String read(int characters) throws IOException;
 
+	/**
+	 * Read a line of data from a file. Reads until a line feed is detected.
+	 *
+	 * @return single line of text
+	 * @throws IOException
+	 *             on access errors
+	 */
 	String readLine() throws IOException;
 
+	/**
+	 * Write data to a file
+	 *
+	 * @param data
+	 *            data to write
+	 * @return <code>true</code> on success
+	 */
 	boolean write(String data);
+
+	/**
+	 * Check if a physical file exists.
+	 *
+	 * @return <code>true</code> when file exists
+	 */
 
 	boolean exists();
 
+	/**
+	 * Create a file.
+	 *
+	 * @param createHierarchy
+	 *            create parent folders if they do not exist
+	 * @return <code>true</code> on success
+	 * @throws Exception
+	 *             on creation errors
+	 */
 	boolean createFile(boolean createHierarchy) throws Exception;
 
+	/**
+	 * Close a file instance.
+	 */
 	void close();
 
 	/**

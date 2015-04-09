b48d49a9532972af86ed90ab69617b84e6de3f0d
Sansovic Domjan <domjan.sansovic@infineon.com>
2015-04-09 18:27:27 +0200

    Bug 463886 - [Charting] Add toolbar button to export graph as image
      Add a toolbar button to save the current XY Chart to an image.
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=463886
    Change-Id: Idf0c3a749420e2538253ddfd4d2c506968e7d7e2
    Signed-off-by: Sansovic Domjan <domjan.sansovic@infineon.com>

---
+++ plugins/org.eclipse.ease.modules.charting/src/org/eclipse/ease/modules/charting/commands/ExportGraph.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Domjan Sansovic and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Domjan Sansovic - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ease.modules.charting.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ease.Logger;
+import org.eclipse.ease.modules.charting.views.ChartView;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ExportGraph extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
+		if (activePart instanceof ChartView) {
+			try {
+				((ChartView) activePart).getChart().export(null, false);
+			} catch (Exception e) {
+				MessageDialog.openError(HandlerUtil.getActiveShell(event), "Export Graph", "Could not export Graph to PNG file");
+				Logger.logError("Could not export Graph to PNG file", e);
+			}
+		}
+		return null;
+	}
+
+}
